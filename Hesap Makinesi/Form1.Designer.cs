f570dbc52a33ee49022d08f19966b71789706b09
Aidin-CodeMan <jafarzadehaidin5@gmail.com>
2022-06-12 21:13:55 +0300

    Proje dosyası ekle.

---
+++ Hesap Makinesi/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Hesap_Makinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.bArti = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.bEksi = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.bCarp = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.bBol = new System.Windows.Forms.Button();
+            this.s5 = new System.Windows.Forms.Button();
+            this.bSil = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 58);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 51);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b1.Location = new System.Drawing.Point(12, 145);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(61, 55);
+            this.b1.TabIndex = 2;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b2.Location = new System.Drawing.Point(79, 145);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(61, 55);
+            this.b2.TabIndex = 3;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b3.Location = new System.Drawing.Point(146, 145);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(61, 55);
+            this.b3.TabIndex = 4;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            // 
+            // bArti
+            // 
+            this.bArti.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bArti.Location = new System.Drawing.Point(213, 145);
+            this.bArti.Name = "bArti";
+            this.bArti.Size = new System.Drawing.Size(61, 55);
+            this.bArti.TabIndex = 5;
+            this.bArti.Text = "+";
+            this.bArti.UseVisualStyleBackColor = true;
+            // 
+            // b4
+            // 
+            this.b4.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b4.Location = new System.Drawing.Point(12, 206);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(61, 55);
+            this.b4.TabIndex = 6;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            // 
+            // b5
+            // 
+            this.b5.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b5.Location = new System.Drawing.Point(79, 206);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(61, 55);
+            this.b5.TabIndex = 7;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            // 
+            // b6
+            // 
+            this.b6.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b6.Location = new System.Drawing.Point(146, 206);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(61, 55);
+            this.b6.TabIndex = 8;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            // 
+            // bEksi
+            // 
+            this.bEksi.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bEksi.Location = new System.Drawing.Point(213, 206);
+            this.bEksi.Name = "bEksi";
+            this.bEksi.Size = new System.Drawing.Size(61, 55);
+            this.bEksi.TabIndex = 9;
+            this.bEksi.Text = "-";
+            this.bEksi.UseVisualStyleBackColor = true;
+            // 
+            // b7
+            // 
+            this.b7.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b7.Location = new System.Drawing.Point(12, 267);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(61, 55);
+            this.b7.TabIndex = 10;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            // 
+            // b8
+            // 
+            this.b8.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b8.Location = new System.Drawing.Point(79, 267);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(61, 55);
+            this.b8.TabIndex = 11;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            // 
+            // b9
+            // 
+            this.b9.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b9.Location = new System.Drawing.Point(146, 267);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(61, 55);
+            this.b9.TabIndex = 12;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            // 
+            // bCarp
+            // 
+            this.bCarp.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bCarp.Location = new System.Drawing.Point(213, 267);
+            this.bCarp.Name = "bCarp";
+            this.bCarp.Size = new System.Drawing.Size(61, 55);
+            this.bCarp.TabIndex = 13;
+            this.bCarp.Text = "x";
+            this.bCarp.UseVisualStyleBackColor = true;
+            // 
+            // b0
+            // 
+            this.b0.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b0.Location = new System.Drawing.Point(79, 328);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(61, 55);
+            this.b0.TabIndex = 14;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            // 
+            // bBol
+            // 
+            this.bBol.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bBol.Location = new System.Drawing.Point(213, 328);
+            this.bBol.Name = "bBol";
+            this.bBol.Size = new System.Drawing.Size(61, 55);
+            this.bBol.TabIndex = 15;
+            this.bBol.Text = "/";
+            this.bBol.UseVisualStyleBackColor = true;
+            this.bBol.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // s5
+            // 
+            this.s5.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.s5.Location = new System.Drawing.Point(149, 328);
+            this.s5.Name = "s5";
+            this.s5.Size = new System.Drawing.Size(61, 55);
+            this.s5.TabIndex = 16;
+            this.s5.Text = "1";
+            this.s5.UseVisualStyleBackColor = true;
+            // 
+            // bSil
+            // 
+            this.bSil.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bSil.Location = new System.Drawing.Point(12, 328);
+            this.bSil.Name = "bSil";
+            this.bSil.Size = new System.Drawing.Size(61, 55);
+            this.bSil.TabIndex = 17;
+            this.bSil.Text = "C";
+            this.bSil.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 401);
+            this.Controls.Add(this.bSil);
+            this.Controls.Add(this.s5);
+            this.Controls.Add(this.bBol);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.bCarp);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.bEksi);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.bArti);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Button b1;
+        private Button b2;
+        private Button b3;
+        private Button bArti;
+        private Button b4;
+        private Button b5;
+        private Button b6;
+        private Button bEksi;
+        private Button b7;
+        private Button b8;
+        private Button b9;
+        private Button bCarp;
+        private Button b0;
+        private Button bBol;
+        private Button s5;
+        private Button bSil;
+    }
+}
